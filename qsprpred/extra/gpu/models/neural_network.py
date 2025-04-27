12695f5b42beacb3ede1c5e7afd3432001ec3e74
krynekt <krynekt@gmail.com>
2025-04-27 20:56:58 +0000

    reset_gpu

--- qsprpred/extra/gpu/models/neural_network.py
+++ qsprpred/extra/gpu/models/neural_network.py
@@ -327,6 +327,9 @@ class Base(nn.Module):
 
     @staticmethod
     def set_seed(seed):
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        np.random.seed(seed)
         torch.manual_seed(seed)
         torch.cuda.manual_seed(seed)
 
